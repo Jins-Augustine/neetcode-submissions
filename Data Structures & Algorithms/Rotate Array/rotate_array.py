917daf64f6a43818d72df9cf720d4e18e8f983d3
Jins <145307894+Jins-Augustine@users.noreply.github.com>
2026-06-24 18:39:44 +0530

    Implement array rotation to right by K steps

--- Data Structures & Algorithms/Rotate Array/rotate_array.py
+++ Data Structures & Algorithms/Rotate Array/rotate_array.py
@@ -1,1 +1,24 @@
-
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        left = 0
+        right = len(nums) - 1
+        while(left<right):
+            nums[right],nums[left] = nums[left] , nums[right]
+            left +=1
+            right -= 1
+        left = 0
+        right = k-1
+        while(left<right):
+            nums[right],nums[left] = nums[left],nums[right]
+            left += 1
+            right -= 1
+        left = k
+        right = len(nums) - 1
+        while(left<right):
+            nums[right],nums[left] = nums[left] , nums[right]
+            left +=1
+            right -= 1
